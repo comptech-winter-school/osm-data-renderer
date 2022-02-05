02963121c49198fc868b284a6f4ee6abd42a602a
cirno42 <4413654@gmail.com>
2022-02-05 21:52:18 +0700

    Little fix with deploy

--- server/deploy.sh
+++ server/deploy.sh
@@ -2,7 +2,7 @@
 
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o osm-renderer-server ./cmd/server/main.go
 ssh osm-server@159.223.28.18 'sudo systemctl stop server'
-scp ./osm-renderer-server osm-server@159.223.28.18:/home/osm-server/server
-scp ./.env osm-server@159.223.28.18:/home/osm-server/server
+scp ./osm-renderer-server osm-server@159.223.28.18:/home/osm-server/server/server
+scp ./.env osm-server@159.223.28.18:/home/osm-server/server/server
 ssh osm-server@159.223.28.18 'sudo systemctl start server'
 
